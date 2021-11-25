74571e9b106ec489d168e9a0814dae4c50ad09c7
victor Jose Calderon <victorjosecaderon@victors-MacBook-Air-2.local>
2021-11-25 17:55:13 -0400

    day1

---
+++ ISBN/EmptyClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace ISBN
+{
+    public class EmptyClass
+    {
+        public static bool Visbn(int[] number)
+        {
+            int multiply = 0;
+            int sum = 0;
+
+            if (number.Length == 10)
+            {
+                int position = 10;
+
+                for (int i = 0; i < number.Length; i++)
+                {
+                    multiply = number[i] * position;
+                    sum += multiply;
+                    position--;
+                    multiply = 0;
+                }
+
+                if (sum % 11 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (number.Length == 13)
+            {
+
+                for (int i = 0; i < number.Length; i++)
+                {
+                    if (number[i] % 2 == 0)
+                    {
+                        multiply = number[i] * 1;
+                    }
+                    else
+                    {
+                        multiply = number[i] * 3;
+                    }
+                    sum += multiply;
+                    multiply = 0;
+                }
+
+                if (sum % 10 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ISBN/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ISBN
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] number = new int[] { 9, 7, 8, 0, 7, 1, 6, 7, 0, 3, 4, 4, 0 };
+
+            bool result = EmptyClass.Visbn(number);
+
+            Assert.AreEqual(result,true);
+
+        }
+    }
+}
